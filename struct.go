fe7f6258b219c8d048d65564202655f70a29006b
snuffon <43469272+snuffon@users.noreply.github.com>
2019-02-24 14:28:56 +0100

    struct added

---
+++ struct.go
@@ -0,0 +1,32 @@
+package nylechain
+
+/*
+This holds the messages used to communicate with the service over the network.
+*/
+
+import (
+	"go.dedis.ch/onet/v3"
+	"go.dedis.ch/onet/v3/network"
+)
+
+func init() {
+	network.RegisterMessages(
+		CoSi{}, CoSiReply{},
+	)
+}
+
+const (
+	// ErrorParse indicates an error while parsing the protobuf-file.
+	ErrorParse = iota + 4000
+)
+
+// CoSi will run the cosi protocol on the roster
+type CoSi struct {
+	Roster  *onet.Roster
+	Message []byte
+}
+
+// CoSiReply returns the signature
+type CoSiReply struct {
+	Signature []byte
+}
